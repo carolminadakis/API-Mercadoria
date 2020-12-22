bc2f76e85d58bcbd1964faa28dfd111732a51155
Ana Carolina Minadakis <carolminadakis@gmail.com>
2020-12-22 12:17:29 -0300

    Corrigindo erros

--- src/main/java/com/api/mercadoria/controller/MercadoriaController.java
+++ src/main/java/com/api/mercadoria/controller/MercadoriaController.java
@@ -1,6 +1,7 @@
 package com.api.mercadoria.controller;
 
 import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
--- src/main/java/com/api/mercadoria/model/Mercadoria.java
+++ src/main/java/com/api/mercadoria/model/Mercadoria.java
@@ -55,5 +55,29 @@ public class Mercadoria {
 	public void setPreco(BigDecimal preco) {
 		this.preco = preco;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Mercadoria other = (Mercadoria) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
 	
 }
